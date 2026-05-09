d8880ab1fd57c779e42cffa2803d3ea04713a661
Shara-tabassum <tabassumshara4@gmail.com>
2026-05-09 23:14:05 +0600

    game.h

--- CG project/game.h
+++ CG project/game.h
@@ -1,1 +1,17 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "constants.h"
+
+void initStars();
+void setupLevel(int lvl);
+void resetBall(bool waitForLaunch);
+void initGame();
+void applyPerk(PerkType t);
+void spawnParticles(float x,float y,Col3 col,int n = 10);
+void updateParticles(float dt);
+void update(float dt);
+void fireBullets();
+
+#endif
 
