35830fe9a104968d11e3eda918457b33d3486440
Towhidul Islam Yeamin <towhidul.islam.yeamin038@gmail.com>
2026-05-09 23:10:05 +0600

    globals.cpp

--- CG project/globals.cpp
+++ CG project/globals.cpp
@@ -1,1 +1,42 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
 
+#include <vector>
+#include "constants.h"
+
+// Game state
+extern GameState   gState;
+extern int         menuSel;
+
+// Gameplay objects
+extern float       padX;
+extern float       padW;
+extern Ball        ball;
+
+extern std::vector<Block>    blocks;
+extern std::vector<Perk>     perks;
+extern std::vector<Bullet>   bullets;
+extern std::vector<Particle> particles;
+extern std::vector<Star>     stars;
+extern std::vector<HSEntry>  highScores;
+
+// Numbers
+extern int   lives;
+extern int   score;
+extern int   level;
+extern int   totalBreak;
+extern int   broken;
+extern float elapsed;
+extern float speedMult;
+
+// Perk timers
+extern float fireTimer;
+extern float shootTimer;
+extern float shootCD;
+
+// Input flags
+extern bool  leftKey;
+extern bool  rightKey;
+extern int   lastMs;
+
+#endif
