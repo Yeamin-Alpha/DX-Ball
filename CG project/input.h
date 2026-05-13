056697dd15c81b23c175c5cc8a542f29f94e7372
Towhidul Islam Yeamin <towhidul.islam.yeamin038@gmail.com>
2026-05-13 13:36:56 +0600

    input.h

--- CG project/input.h
+++ CG project/input.h
@@ -1,1 +1,11 @@
 
+#ifndef INPUT_H
+#define INPUT_H
+
+void keyDown(unsigned char k,int x,int y);
+void specialDown(int k,int x,int y);
+void specialUp(int k,int x,int y);
+void mouseMove(int x,int y);
+void mouseClick(int btn,int st,int x,int y);
+
+#endif
